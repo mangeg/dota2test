8a4544b9fb1a8103797057ae4b3af1ff355c55fa
Magnus Gideryd <mgideryd@gmail.com>
2015-05-17 01:03:05 +0200

    - New base

---
+++ Dota2Test/src/Dota2.WebApp/Startup.cs
@@ -0,0 +1,17 @@
+﻿namespace Dota2.WebApp
+{
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Http;
+    using Microsoft.Framework.DependencyInjection;
+
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices( IServiceCollection services ) {}
+
+        public void Configure( IApplicationBuilder app )
+        {
+            app.Run( async ( context ) => { await context.Response.WriteAsync( "Hello World!" ); } );
+        }
+    }
+}
