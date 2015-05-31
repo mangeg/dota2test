a145d6d1d753c078466b8dbed740386c99b9a6f6
Magnus Gideryd <mgideryd@gmail.com>
2015-05-31 07:48:18 +0200

    Work again

--- Dota2Test/tests/WebApplication1/Scripts/app.ts
+++
@@ -1,9 +0,0 @@
-﻿export module TestModule {
-    export function testFunc( input: string ) {
-        return input;
-    }
-
-    export class TestClass {
-        name: string = "Hello";
-    }
-}
--- Dota2Test/tests/WebApplication1/Scripts/test.ts
+++
@@ -1,5 +0,0 @@
-﻿import {TestModule} from "app"
-//alert( test.TestModule.testFunc( "sdasd" ) );
-alert(TestModule.testFunc("asfasf"));
-var instance = new TestModule.TestClass;
-alert( instance.name );
