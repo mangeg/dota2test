cc481913a2b82955b1171f450d0a2f8ba757e71e
Magnus Gideryd <mgideryd@gmail.com>
2015-06-03 01:41:26 +0200

    Do

---
+++ Dota2Test/src/Dota2.WebApp/gulpfile.config.js
@@ -0,0 +1,22 @@
+﻿var fs = require( "fs-extra" );
+var project = fs.readJSONSync( "./project.json" );
+
+var GulpConfig = ( function() {
+    function gulpConfig() {
+        this.www = project.webroot;
+        this.jsRoot = this.www + "/js";
+        this.jsLib = this.jsRoot + "/lib";
+        this.jsApp = this.jsRoot + "/app";
+        this.bower = "./bower_components/";
+        this.tsSource = "./App";
+
+        this.allTs = this.tsSource + "/**/*.ts";
+
+        this.tsTypings = "./Tools/Typings";
+        this.libTypeDefs = this.tsTypings + "/**/*.ts";
+        this.appTypeDef = this.tsTypings + "/app.d.ts";
+    }
+
+    return gulpConfig;
+} )();
+module.exports = GulpConfig;
---
+++ Dota2Test/src/Dota2.WebApp/wwwroot/init.js
@@ -0,0 +1,20 @@
+﻿require.config( {
+    //By default load any module IDs from js/lib
+    baseUrl: "app/",
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        lib: "../lib",
+    }
+} );
+
+requirejs( ["lib/angular/angular"], function() {
+    require( ["app"], function() {
+        angular.element( document ).ready( function() {
+            angular.bootstrap( document, ['dotaApp'] );
+        } );
+    } );
+} );
